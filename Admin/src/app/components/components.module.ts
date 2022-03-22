594e10721d0461c5af7412c2212e9feb21b1e91a
villacisa <cf20adrian.villacis@iesjoandaustria.org>
2022-03-22 19:07:37 +0100

    cambios

--- Admin/src/app/components/components.module.ts
+++ Admin/src/app/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
   imports: [
     CommonModule
   ]
