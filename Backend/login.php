07a84d4495d89e210e5483fb1164492c7d71eb77
Marc <cf20marc.roset@gmail.com>
2022-03-22 19:42:13 +0100

    creado el fichero login.php

--- Backend/login.php
+++ Backend/login.php
@@ -5,5 +5,17 @@
     
     $con = new mysqli();
     $con->connect('localhost','marc','marc','Tinder');
-    
+
+    if($con>connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+    $res=$con->query("select * from users where '".$email."'= 'mail' and 'pwd' = '".$pwd."'");
+    if(mysqli_num_rows($res)==1) {
+        echo 'ok';
+    }
+    else {
+        echo "no hay coincidencias";
+    }
+
 ?>
